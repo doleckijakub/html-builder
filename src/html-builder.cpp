3fc503b5d157435a300dd9c5dc44cd38a2a16d25
Jakub Dolecki <kubus.dolecki@gmail.com>
2023-06-26 14:07:08 +0200

    Impelemnted rudimentary html builder

---
+++ src/html-builder.cpp
@@ -0,0 +1,103 @@
+#include "html-builder.hpp"
+
+namespace html {
+
+element::element(const std::string &tag, const std::string &text, bool paired)
+	: tag(tag), inner_text(text), paired(paired) {
+}
+
+element &element::addAttribute(const std::string &name, const std::string &value) {
+	attributes.push_back({name, value});
+	return *this;
+}
+
+element &element::operator<<(element el) {
+	children.push_back(el);
+	return *this;
+}
+
+element &element::operator<<(std::string content) {
+	return *this << text(content);
+}
+
+void element::dumpToString(std::string &target) const {
+	if (tag.empty()) {
+		target += inner_text;
+	} else {
+		// start tag
+		target += "<";
+		target += tag;
+
+		for (const auto &attr : attributes) {
+			target += " ";
+			target += attr.name;
+			if (!attr.value.empty()) {
+				target += "=\"";
+				target += attr.value;
+				target += "\"";
+			}
+		}
+
+		if (!paired)
+			target += " /";
+		target += ">";
+
+		// content
+		target += inner_text;
+		for (const auto &child : children) {
+			child.dumpToString(target);
+		}
+
+		// end tag
+		if (paired) {
+			target += "</";
+			target += tag;
+			target += ">";
+		}
+	}
+}
+
+text::text(std::string content) : element("", content, false) {
+}
+
+html::html() : element("html", "", true) {
+}
+head::head() : element("head", "", true) {
+}
+body::body() : element("body", "", true) {
+}
+span::span() : element("span", "", true) {
+}
+h1::h1() : element("h1", "", true) {
+}
+h2::h2() : element("h2", "", true) {
+}
+h3::h3() : element("h3", "", true) {
+}
+h4::h4() : element("h4", "", true) {
+}
+h5::h5() : element("h5", "", true) {
+}
+h6::h6() : element("h6", "", true) {
+}
+p::p() : element("p", "", true) {
+}
+b::b() : element("b", "", true) {
+}
+i::i() : element("i", "", true) {
+}
+a::a() : element("a", "", true) {
+}
+
+input::input() : element("input", "", false) {
+}
+link::link() : element("link", "", false) {
+}
+meta::meta() : element("meta", "", false) {
+}
+img::img() : element("img", "", false) {
+}
+br::br() : element("br", "", false) {
+}
+
+} // namespace html
---
+++ src/html-builder.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace html {
+
+struct attribute {
+	std::string name, value;
+};
+
+class element {
+  protected:
+	std::string tag, inner_text;
+	bool paired;
+	std::vector<element> children;
+	std::vector<attribute> attributes;
+
+	explicit element(const std::string &tag, const std::string &text = "", bool paired = true);
+
+  public:
+	element &addAttribute(const std::string &name, const std::string &value);
+
+	element &operator<<(element el);
+
+	element &operator<<(std::string);
+
+	void dumpToString(std::string &) const;
+};
+
+struct text : public element {
+	text(std::string content);
+};
+
+struct html : public element {
+	explicit html();
+};
+struct head : public element {
+	explicit head();
+};
+struct body : public element {
+	explicit body();
+};
+struct span : public element {
+	explicit span();
+};
+struct h1 : public element {
+	explicit h1();
+};
+struct h2 : public element {
+	explicit h2();
+};
+struct h3 : public element {
+	explicit h3();
+};
+struct h4 : public element {
+	explicit h4();
+};
+struct h5 : public element {
+	explicit h5();
+};
+struct h6 : public element {
+	explicit h6();
+};
+struct p : public element {
+	explicit p();
+};
+struct b : public element {
+	explicit b();
+};
+struct i : public element {
+	explicit i();
+};
+struct a : public element {
+	explicit a();
+};
+
+struct input : public element {
+	explicit input();
+};
+struct link : public element {
+	explicit link();
+};
+struct meta : public element {
+	explicit meta();
+};
+struct img : public element {
+	explicit img();
+};
+struct br : public element {
+	explicit br();
+};
+
+} // namespace html
